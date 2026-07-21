9b28a8a8e69d44a3f374cb01881511fc90c65d98
Sudesh Kharel <sudeshkharel15@gmail.com>
2026-07-21 16:41:57 +0545

    hooked class-mitii-admin-menu.php file into  main plugin file

--- mitii-booking.php
+++ mitii-booking.php
@@ -10,8 +10,11 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 define( 'MITII_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'MITII_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
+require_once MITII_PLUGIN_DIR . 'includes/admin/class-mitii-admin-menu.php';
 require_once MITII_PLUGIN_DIR . 'includes/class-mitii-activator.php';
 require_once MITII_PLUGIN_DIR . 'includes/api/class-services-controller.php';
 
 register_activation_hook( __FILE__, array( 'Mitii_Activator', 'activate' ) );
 add_action( 'rest_api_init', array( 'Mitii_Services_Controller', 'register_routes' ) );
+add_action( 'admin_menu', array( 'Mitii_Admin_Menu', 'register' ) );
+add_action( 'admin_enqueue_scripts', array( 'Mitii_Admin_Menu', 'enqueue_assets' ) );
