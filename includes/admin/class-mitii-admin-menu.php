72c745aeb7bda7fde006bb54f5bf1db40f2bd4c5
Sudesh Kharel <sudeshkharel15@gmail.com>
2026-07-22 12:29:42 +0545

    updated enqueue assets

--- includes/admin/class-mitii-admin-menu.php
+++ includes/admin/class-mitii-admin-menu.php
@@ -17,19 +17,23 @@ class Mitii_Admin_Menu {
         echo '<div id="mitii-admin-root"></div>';
     }
 
-    public static function enqueue_assets( $hook ) {
-        if ( $hook !== 'toplevel_page_mitii-booking' ) {
-            return;
-        }
-
-        $asset_file = include MITII_PLUGIN_DIR . 'build/admin.asset.php';
-
-        wp_enqueue_script(
-            'mitii-admin',
-            MITII_PLUGIN_URL . 'build/admin.js',
-            $asset_file['dependencies'],
-            $asset_file['version'],
-            true
-        );
+   public static function enqueue_assets( $hook ) {
+    if ( $hook !== 'toplevel_page_mitii-booking' ) {
+        return;
     }
+
+    $asset_file = include MITII_PLUGIN_DIR . 'build/admin.asset.php';
+
+    wp_enqueue_script(
+        'mitii-admin',
+        MITII_PLUGIN_URL . 'build/admin.js',
+        $asset_file['dependencies'],
+        $asset_file['version'],
+        true
+    );
+
+    wp_localize_script( 'mitii-admin', 'mitiiAdminData', array(
+        'nonce' => wp_create_nonce( 'wp_rest' ),
+    ) );
+}
 }
