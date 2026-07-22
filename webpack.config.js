68c44cf1c329d16f6124e74f56bd47631a336437
Sudesh Kharel <sudeshkharel15@gmail.com>
2026-07-22 14:59:42 +0545

    added customer portal entry

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,13 @@ const path = require( 'path' );
 module.exports = {
     ...defaultConfig,
    entry: {
+   entry: {
     'admin-bookings': path.resolve( __dirname, 'src/admin/bookings', 'index.tsx' ),
     'admin-services': path.resolve( __dirname, 'src/admin/services', 'index.tsx' ),
     'admin-staff': path.resolve( __dirname, 'src/admin/staff', 'index.tsx' ),
     'public-widget': path.resolve( __dirname, 'src/public-widget', 'index.tsx' ),
-},
+    'customer-portal': path.resolve( __dirname, 'src/customer-portal', 'index.tsx' ),
+},},
     output: {
         ...defaultConfig.output,
         path: path.resolve( process.cwd(), 'build' ),
