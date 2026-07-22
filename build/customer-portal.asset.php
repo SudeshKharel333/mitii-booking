f8523d57a1cba61222f78729b0542e09f84d4f80
Sudesh Kharel <sudeshkharel15@gmail.com>
2026-07-22 15:28:23 +0545

    removed the extra 'entry'

---
+++ build/customer-portal.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '91626b704fb02247ec34');
---
+++ includes/class-mitii-customer-portal-shortcode.php
@@ -0,0 +1,29 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class Mitii_Customer_Portal_Shortcode {
+
+    public static function register() {
+        add_shortcode( 'mitii_customer_portal', array( __CLASS__, 'render' ) );
+    }
+
+    public static function render() {
+        return '<div id="mitii-customer-portal-root"></div>';
+    }
+
+    public static function enqueue_assets() {
+        if ( ! is_singular() || ! has_shortcode( get_post()->post_content, 'mitii_customer_portal' ) ) {
+            return;
+        }
+
+        $asset_file = include MITII_PLUGIN_DIR . 'build/customer-portal.asset.php';
+
+        wp_enqueue_script(
+            'mitii-customer-portal',
+            MITII_PLUGIN_URL . 'build/customer-portal.js',
+            $asset_file['dependencies'],
+            $asset_file['version'],
+            true
+        );
+    }
+}
