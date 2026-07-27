137fd70a83aee86eb46a039c900c252027d37a06
Sudesh Kharel <sudeshkharel15@gmail.com>
2026-07-27 13:42:58 +0545

    hooked staff_first shortcode into main file

--- mitii-booking.php
+++ mitii-booking.php
@@ -18,6 +18,7 @@ require_once MITII_PLUGIN_DIR . 'includes/api/class-bookings-controller.php';
 require_once MITII_PLUGIN_DIR . 'includes/class-mitii-shortcode.php';
 require_once MITII_PLUGIN_DIR . 'includes/api/class-customer-auth-controller.php';
 require_once MITII_PLUGIN_DIR . 'includes/class-mitii-customer-session.php';
+require_once MITII_PLUGIN_DIR . 'includes/class-mitii-staff-first-shortcode.php';
 
 register_activation_hook( __FILE__, array( 'Mitii_Activator', 'activate' ) );
 require_once MITII_PLUGIN_DIR . 'includes/class-mitii-customer-portal-shortcode.php';
@@ -33,3 +34,5 @@ add_action( 'admin_menu', array( 'Mitii_Admin_Menu', 'register' ) );
 add_action( 'admin_enqueue_scripts', array( 'Mitii_Admin_Menu', 'enqueue_assets' ) );
 add_action( 'init', array( 'Mitii_Shortcode', 'register' ) );
 add_action( 'wp_enqueue_scripts', array( 'Mitii_Shortcode', 'enqueue_assets' ) );
+add_action( 'init', array( 'Mitii_Staff_First_Shortcode', 'register' ) );
+add_action( 'wp_enqueue_scripts', array( 'Mitii_Staff_First_Shortcode', 'enqueue_assets' ) );
